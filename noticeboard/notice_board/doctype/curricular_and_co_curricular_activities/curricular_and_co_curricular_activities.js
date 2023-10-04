1d3e32c723812f8e9683f9b0768406815fbe8724
SatinderpalSingh <satinder.goraya91@gmail.com>
2023-10-05 00:32:20 +0530

    added owner information in noticeboard/add a new doctype for cocurricular activities as per requirements of dean DSW

---
+++ noticeboard/notice_board/doctype/curricular_and_co_curricular_activities/curricular_and_co_curricular_activities.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, satinderpal singh and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Curricular and Co-curricular Activities', {
+	// refresh: function(frm) {
+
+	// }
+});
